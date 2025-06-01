922d2cfc3fc33294019a919d6a3543ce1acf133c
SWKHansala <hansalawaduge@gmail.com>
2025-06-01 22:26:49 +0530

    Initial commit: Added basic HTML structure with navigation bar

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+ <meta charset="utf-8">
+ <title>Student Management System</title>
+</head>
+<body>
+ <nav>
+  <label>W-School</label>
+  <ul>
+   <li><a href="">Home</a></li>
+   <li><a href="">Contact</a></li>
+   <li><a href="">Admission</a></li>
+   <li><a href="">Login</a></li>
+  </ul>
+ </nav>
+</bodY>
+</html>
