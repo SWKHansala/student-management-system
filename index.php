b9f7f7388994de3b4c96eec373c437bcb42e92b0
SWKHansala <hansalawaduge@gmail.com>
2025-06-05 12:47:30 +0530

    Updated index.php and style.css, added new images for classroom

--- index.php
+++ index.php
@@ -2,16 +2,56 @@
 <head>
  <meta charset="utf-8">
  <title>Student Management System</title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
  <nav>
-  <label>W-School</label>
+  <label class="logo">W-School</label>
   <ul>
    <li><a href="">Home</a></li>
    <li><a href="">Contact</a></li>
    <li><a href="">Admission</a></li>
-   <li><a href="">Login</a></li>
+   <li><a href="" class="btn btn-success">Login</a></li>
   </ul>
  </nav>
+ <div class="section1">
+    <label class="img_text">We Teach Students With Care</label>
+    <img class="main" src="Classroom-Management-for-an-Effective-Learning-Environment-scaled.jpg" height: 300px>
+ </div>
+<div class="container">
+   <div class="row">
+      <div class="col-md-4">
+         <img class="school_img" src="istockphoto-1338737959-612x612.jpg">
+      </div>
+      <div class="col-md-8">
+         <h1>Welcome to W-School</h1>
+         <p>MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Bangladesh to adopt both Pearson Edexcel and Cambridge curriculum (In O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Bangladesh to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated</p>
+      </div>
+   </div>
+</div> 
+ <center>
+   <h1>Our Teachers</h1>
+ </center>
+ <div class="container">
+   <div class="row">
+      <div class="col-md-4">
+         <img class="teacher" src="istockphoto-944012304-612x612.jpg">
+      </div>
+      <div class="col-md-4">
+         <img class="teacher" src="istockphoto-486325400-612x612.jpg">
+      </div>
+      <div class="col-md-4">
+         <img class="teacher" src="children-at-school-classroom_BtxrKoaHs_thumb.jpg">
+      </div>
+   </div>
+ </div>
 </bodY>
 </html>
